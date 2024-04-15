7709bc5153e023b6d2e522876c255c1ea887d4b7
Sergio Diaz <sadiazcaballero@gmail.com>
2024-04-15 17:53:07 +0200

    Modified quiz.js / question.js 15.04.2024

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,39 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
-
-    // 2. getQuestion()
+    constructor(questions, timeLimit, timeRemaining) {
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+    }
     
-    // 3. moveToNextQuestion()
+    getQuestion() {
+        return this.questions[this.currentQuestionIndex];
+    }
 
-    // 4. shuffleQuestions()
+    moveToNextQuestion() {
+        this.currentQuestionIndex++;
+    }
+    
+    shuffleQuestions() {
+        for (let i = this.questions.length -1; i > 0 ; i--) {
+            let j  = Math.floor(Math.random()*(i + 1));
+            [this.questions[i], this.questions[j]] = [this.questions[j], this.questions[i]];
+        }
+    }
 
-    // 5. checkAnswer(answer)
+    checkAnswer(answer) {
+        const currentQuestion = this.getQuestion();
+        if (answer === currentQuestion.answer) {
+            this.correctAnswers++;
+        }
+    }
 
-    // 6. hasEnded()
+    hasEnded() {
+        if (this.currentQuestionIndex < this.questions.length) {
+            return false;
+        } else if (this.currentQuestionIndex === this.questions.length) {
+        return true;
+    }
+}
 }
